97ff437ded3901d5363a1370572d2ca16848d4db
tknapp5347 <student@it515561.ad.ppsnet>
2015-06-01 09:40:40 -0700

    changes

---
+++ tknapp/js/formscripts.js
@@ -0,0 +1,21 @@
+var form = document.forms.practiceForm;
+
+
+form.addEventListener("submit", displayPerson, false);
+
+
+function displayPerson(event) {
+
+	event.preventDefault();
+
+	var Person = {
+		name: form.catname.value,
+		user: form.username.value,
+		pass: form.password.value,
+	}
+
+	var html = "<p>Your cat's name is: " + Person.name + "</p>" + "<p>Your username is:" + Person.user + "</p>" + "<p>Your password is:" + Person.pass + "</p>";
+
+
+	document.getElementById("displayText").innerHTML = html;
+}
